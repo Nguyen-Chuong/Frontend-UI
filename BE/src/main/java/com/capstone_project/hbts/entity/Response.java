3f8e6b2ec8a8f77f63599a7ef302951aa26f00d3
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-02-25 20:47:09 +0700

    add table response

--- BE/src/main/java/com/capstone_project/hbts/entity/Response.java
+++ BE/src/main/java/com/capstone_project/hbts/entity/Response.java
@@ -8,16 +8,13 @@ import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.sql.Timestamp;
-import java.util.Set;
 
 @ToString
 @AllArgsConstructor
@@ -25,24 +22,16 @@ import java.util.Set;
 @Setter
 @Getter
 @Entity
-@Table(name = "Feedback")
-public class Feedback {
+@Table(name = "Response")
+public class Response {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    // type 0: complain, type 1: refund, type 2: other, v v
-    @Column(name = "type")
-    private int type;
-
     @ManyToOne
-    @JoinColumn(name = "sender_id")
-    private Users sender;
-
-    @ManyToOne
-    @JoinColumn(name = "receiver_id")
-    private Users receiver;
+    @JoinColumn(name = "feedback_id")
+    private Feedback feedback;
 
     @Column(name = "message")
     private String message;
@@ -50,10 +39,10 @@ public class Feedback {
     @Column(name = "modify_date")
     private Timestamp modifyDate;
 
-    @Column(name = "is_completed")
-    private int isProcessed;
+    @Column(name = "user_id")
+    private int userId;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "feedback")
-    private Set<Response> listResponse;
+    @Column(name = "admin_id")
+    private int adminId;
 
 }
