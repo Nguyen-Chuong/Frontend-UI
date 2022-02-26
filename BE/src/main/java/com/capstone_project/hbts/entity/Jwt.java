5c9642b1d09b2d5113e67bc9abefa53498e88f00
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-02-26 16:46:36 +0700

    add table jwt for admin authen

---
+++ BE/src/main/java/com/capstone_project/hbts/entity/Jwt.java
@@ -0,0 +1,30 @@
+package com.capstone_project.hbts.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Jwt")
+public class Jwt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "jwt_token")
+    private String jwt;
+
+}
