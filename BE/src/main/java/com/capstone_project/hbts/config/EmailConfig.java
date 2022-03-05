523bc21003739d194e25c2f353d138ef4e3dc278
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-03-05 11:12:25 +0700

    optimize admin resource import

--- BE/src/main/java/com/capstone_project/hbts/config/EmailConfig.java
+++ BE/src/main/java/com/capstone_project/hbts/config/EmailConfig.java
@@ -1,4 +1,4 @@
-package com.capstone_project.hbts.configurations;
+package com.capstone_project.hbts.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
