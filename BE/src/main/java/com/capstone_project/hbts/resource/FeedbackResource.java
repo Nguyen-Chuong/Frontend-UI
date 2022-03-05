b5864c51294ff1dc7631eac731eb0975489a66bb
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-03-05 16:19:56 +0700

    api get user feedback sent for user

--- BE/src/main/java/com/capstone_project/hbts/resource/FeedbackResource.java
+++ BE/src/main/java/com/capstone_project/hbts/resource/FeedbackResource.java
@@ -6,16 +6,19 @@ import com.capstone_project.hbts.dto.Feedback.FeedbackDTO;
 import com.capstone_project.hbts.request.FeedbackRequest;
 import com.capstone_project.hbts.response.ApiResponse;
 import com.capstone_project.hbts.response.DataPagingResponse;
+import com.capstone_project.hbts.security.jwt.JwtTokenUtil;
 import com.capstone_project.hbts.service.FeedbackService;
 import com.capstone_project.hbts.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +35,12 @@ public class FeedbackResource {
 
     private final UserService userService;
 
-    public FeedbackResource(FeedbackService feedbackService, UserService userService) {
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public FeedbackResource(FeedbackService feedbackService, UserService userService, JwtTokenUtil jwtTokenUtil) {
         this.feedbackService = feedbackService;
         this.userService = userService;
+        this.jwtTokenUtil = jwtTokenUtil;
     }
 
     @PostMapping("/send-feedback")
@@ -55,6 +61,7 @@ public class FeedbackResource {
     }
 
     @GetMapping("/get-all-feedback")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<?> getAllFeedback(@RequestParam(defaultValue = ValidateConstant.PAGE) int page,
                                             @RequestParam(defaultValue = ValidateConstant.PER_PAGE) int pageSize){
         log.info("REST request to get all user's feedback");
@@ -77,6 +84,7 @@ public class FeedbackResource {
     }
 
     @GetMapping("/search-feedback")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<?> searchFeedbackOfAnUser(@RequestParam String username){
         log.info("REST request to get an user's feedback");
         int userId;
@@ -88,6 +96,27 @@ public class FeedbackResource {
                     .body(new ApiResponse<>(400, null,
                             ErrorConstant.ERR_USER_006, ErrorConstant.ERR_USER_006_LABEL));
         }
+
+        try {
+            List<FeedbackDTO> feedbackDTOList = feedbackService.getListAnUserFeedback(userId);
+            return ResponseEntity.ok()
+                    .body(new ApiResponse<>(200, feedbackDTOList,
+                            null, null));
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.badRequest()
+                    .body(new ApiResponse<>(400, null,
+                            ErrorConstant.ERR_000, ErrorConstant.ERR_000_LABEL));
+        }
+    }
+
+    @GetMapping("/feedback")
+    public ResponseEntity<?> getListFeedback(@RequestHeader("Authorization") String jwttoken){
+        log.info("REST request to get user's feedbacks that sent");
+
+        String username = jwtTokenUtil.getUsernameFromToken(jwttoken.substring(7));
+
+        int userId = userService.getUserId(username);
 
         try {
             List<FeedbackDTO> feedbackDTOList = feedbackService.getListAnUserFeedback(userId);
