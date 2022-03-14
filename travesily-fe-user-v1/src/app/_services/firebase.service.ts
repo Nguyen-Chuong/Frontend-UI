5e0025a0271f614e7991029cb8b35745888ee547
hoanbui29 <81216107+hoanbui29@users.noreply.github.com>
2022-03-14 18:06:10 +0700

    Add User Avatar upload
    Add User Avatar upload function

--- travesily-fe-user-v1/src/app/_services/firebase.service.ts
+++ travesily-fe-user-v1/src/app/_services/firebase.service.ts
@@ -16,15 +16,15 @@ export class FirebaseService {
     const filePath = `${this.basePath}/${folder}/${encryptedId}`;
     const storageRef = this.storage.ref(filePath);
     const uploadTask = this.storage.upload(filePath, fileUpload.file);
-    uploadTask.snapshotChanges().pipe(
-      finalize(() => {
-        storageRef.getDownloadURL().subscribe(downloadURL => {
-          fileUpload.url = downloadURL;
-          fileUpload.name = fileUpload.file.name;
-          this.saveFileData(fileUpload);
-        });
-      })
-    ).subscribe();
+    // uploadTask.snapshotChanges().pipe(
+    //   finalize(() => {
+    //     storageRef.getDownloadURL().subscribe(downloadURL => {
+    //       fileUpload.url = downloadURL;
+    //       fileUpload.name = fileUpload.file.name;
+    //       this.saveFileData(fileUpload);
+    //     });
+    //   })
+    // ).subscribe();
     return storageRef.getDownloadURL()
   }
 
@@ -34,25 +34,25 @@ export class FirebaseService {
     return storageRef.getDownloadURL()
   }
 
-  private saveFileData(fileUpload: FileUpload): void {
-    this.db.list(this.basePath).push(fileUpload);
-  }
-  getFiles(numberItems): AngularFireList<FileUpload> {
-    return this.db.list(this.basePath, ref =>
-      ref.limitToLast(numberItems));
-  }
-  deleteFile(fileUpload: FileUpload): void {
-    this.deleteFileDatabase(fileUpload.key)
-      .then(() => {
-        this.deleteFileStorage(fileUpload.name);
-      })
-      .catch(error => console.log(error));
-  }
-  private deleteFileDatabase(key: string): Promise<void> {
-    return this.db.list(this.basePath).remove(key);
-  }
-  private deleteFileStorage(name: string): void {
-    const storageRef = this.storage.ref(this.basePath);
-    storageRef.child(name).delete();
-  }
+  // private saveFileData(fileUpload: FileUpload): void {
+  //   this.db.list(this.basePath).push(fileUpload);
+  // }
+  // getFiles(numberItems): AngularFireList<FileUpload> {
+  //   return this.db.list(this.basePath, ref =>
+  //     ref.limitToLast(numberItems));
+  // }
+  // deleteFile(fileUpload: FileUpload): void {
+  //   this.deleteFileDatabase(fileUpload.key)
+  //     .then(() => {
+  //       this.deleteFileStorage(fileUpload.name);
+  //     })
+  //     .catch(error => console.log(error));
+  // }
+  // private deleteFileDatabase(key: string): Promise<void> {
+  //   return this.db.list(this.basePath).remove(key);
+  // }
+  // private deleteFileStorage(name: string): void {
+  //   const storageRef = this.storage.ref(this.basePath);
+  //   storageRef.child(name).delete();
+  // }
 }
--- travesily-fe-user-v1/src/app/components/master/body/body-user/user-profile/user-profile-details/user-profile-details-info/user-profile-details-info.component.ts
+++ travesily-fe-user-v1/src/app/components/master/body/body-user/user-profile/user-profile-details/user-profile-details-info/user-profile-details-info.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Account} from "../../../../../../../_models/account";
+import {FileUpload} from "../../../../../../../_models/file-upload";
+import {FirebaseService} from "../../../../../../../_services/firebase.service";
+import {AuthService} from "../../../../../../../_services/auth.service";
+import {Router} from "@angular/router";
+import {finalize, first} from "rxjs";
 
 @Component({
   selector: 'app-user-profile-details-info',
@@ -9,13 +14,17 @@ import {Account} from "../../../../../../../_models/account";
 export class UserProfileDetailsInfoComponent implements OnInit {
   @Input() account: Account
   @Input() title: string | undefined
-  @Input() avatar: string | undefined
+  @Input() hasAvatar: boolean | undefined = false
   @Input() detail: string | undefined
   @Input() isAdd: boolean | undefined
   @Input() editType: string | undefined
   isEditShow: boolean = false
 
-  constructor() {
+  selectedFiles: FileList;
+  currentFileUpload: FileUpload;
+  showImage: string
+
+  constructor(private firebaseService: FirebaseService, private authService: AuthService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -25,4 +34,50 @@ export class UserProfileDetailsInfoComponent implements OnInit {
     this.isEditShow = !this.isEditShow
   }
 
+  changeAvatar() {
+
+  }
+
+  selectFile(event): void {
+    this.selectedFiles = event.target.files;
+    const [file] = event.target.files
+    if (file) {
+      document.getElementById('preview-image')['src'] = URL.createObjectURL(file)
+    }
+  }
+
+  upload(): void {
+    if (this.selectedFiles) {
+      const file = this.selectedFiles.item(0);
+      this.selectedFiles = undefined;
+      this.currentFileUpload = new FileUpload(file);
+      this.firebaseService.pushFileToStorage(this.currentFileUpload, 'accounts', this.account.id).subscribe(
+        rs => {
+          this.account.avatar = rs
+          this.authService.update(this.account).subscribe(rs => {
+            this.authService.getProfile().pipe(first()).subscribe(account => {
+              this.account = account['data']
+            },error => {
+              console.log(error)
+            },
+              () =>{
+                this.router.navigateByUrl('/user').then(() => {
+                  this.router.navigateByUrl('/user/profile').then(() => {
+                    window.location.reload()
+                  })
+                });
+                // window.location.reload()
+              })
+
+          })
+        },
+        error => {
+          console.log(error);
+        },
+        () => {
+
+        });
+
+    }
+  }
 }
