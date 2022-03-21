0dd01effa770041772327ad65d60136fb4a06911
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-03-22 00:03:54 +0700

    reverse config production env for admin

--- travesily-fe-admin-v1/src/environments/environment.prod.ts
+++ travesily-fe-admin-v1/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL: 'https://capstone-hbts.herokuapp.com/api/v1',
-  // API_URL: 'http://localhost:8080/api/v1',
+  // API_URL: 'https://capstone-hbts.herokuapp.com/api/v1',
+  API_URL: 'http://localhost:8080/api/v1',
   production: true,
 };
