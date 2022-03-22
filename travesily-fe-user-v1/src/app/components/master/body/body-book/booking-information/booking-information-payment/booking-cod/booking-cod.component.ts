c840d2de1fa1a507c0d1ac2fefbfb7fb018f0a22
hoanbui29 <81216107+hoanbui29@users.noreply.github.com>
2022-03-22 21:47:52 +0700

    Add COD method
    Add booking COD method

--- travesily-fe-user-v1/src/app/components/master/body/body-book/booking-information/booking-information-payment/booking-cod/booking-cod.component.ts
+++ travesily-fe-user-v1/src/app/components/master/body/body-book/booking-information/booking-information-payment/booking-cod/booking-cod.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {BookingService} from "../../../../../../../_services/booking.service";
+import {Booking} from "../../../../../../../_models/booking";
+import {CryptoService} from "../../../../../../../_services/crypto.service";
 
 @Component({
   selector: 'app-booking-cod',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking-cod.component.scss']
 })
 export class BookingCodComponent implements OnInit {
+  booking: Booking = new Booking()
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private bookingService: BookingService,
+              private cryptoService: CryptoService) {
+    this.activatedRoute.queryParams.subscribe({
+      next: value => {
+        this.bookingService.getBookingById(value['bookingId']).subscribe({
+          next: booking => {
+            this.booking = booking['data']
+          }
+        })
+      }
+    })
+  }
+
 
   ngOnInit(): void {
   }
 
+  proceed() {
+    this.bookingService.updateBookingType(this.cryptoService.set('06052000', this.booking.id), 2).subscribe({
+      next: value => {
+
+      }
+    })
+  }
 }
