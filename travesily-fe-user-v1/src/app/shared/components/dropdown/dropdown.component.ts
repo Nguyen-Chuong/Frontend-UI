a60423d4cdef16c72b613e111cf9844ffbadac57
hoanbui29 <81216107+hoanbui29@users.noreply.github.com>
2022-03-27 13:30:28 +0700

    Fix dropdown componet
    Fix dropdown component minus function can be negative

--- travesily-fe-user-v1/src/app/shared/components/dropdown/dropdown.component.ts
+++ travesily-fe-user-v1/src/app/shared/components/dropdown/dropdown.component.ts
@@ -32,7 +32,7 @@ export class DropdownComponent implements OnInit {
       this.guestNumberEvent.emit(this.guestNum)
     } else if (type === 2) {
       this.roomNum = this.roomNum + 1
-      if(this.roomNum > this.guestNum)
+      if (this.roomNum > this.guestNum)
         this.guestNum = this.guestNum + 1
       this.roomNumberEvent.emit(this.roomNum)
       this.guestNumberEvent.emit(this.guestNum)
@@ -41,16 +41,17 @@ export class DropdownComponent implements OnInit {
 
 // 1 = guestNumber, 2 = roomNumber
   toggleNumberMinus(type: number) {
-    if(type === 1) {
-      this.guestNum = this.guestNum - 1
-      if(this.guestNum < this.roomNum)
+    if (this.guestNum > 1 && this.roomNum > 1) {
+      if (type === 1) {
+        this.guestNum = this.guestNum - 1
+        if (this.guestNum < this.roomNum)
+          this.roomNum = this.roomNum - 1
+        this.guestNumberEvent.emit(this.guestNum)
+        this.roomNumberEvent.emit(this.roomNum)
+      } else if (type === 2) {
         this.roomNum = this.roomNum - 1
-      this.guestNumberEvent.emit(this.guestNum)
-      this.roomNumberEvent.emit(this.roomNum)
-    }
-     else if(type === 2){
-      this.roomNum = this.roomNum - 1
-      this.roomNumberEvent.emit(this.roomNum)
+        this.roomNumberEvent.emit(this.roomNum)
+      }
     }
   }
 }
