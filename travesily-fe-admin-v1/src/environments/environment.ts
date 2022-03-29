e949d33bc278c887c8b0cb5461e0e3617f13cb2a
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-03-29 20:25:50 +0700

    fix product env

--- travesily-fe-admin-v1/src/environments/environment.ts
+++ travesily-fe-admin-v1/src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   firebase: {
     projectId: 'capstone-343210',
     appId: '1:1022240410262:web:daaa55b157824cff2a90b7',
-    databaseURL: 'https://capstone-343210-default-rtdb.asia-southeast1.firebasedatabase.app',
+    databaseURL:
+      'https://capstone-343210-default-rtdb.asia-southeast1.firebasedatabase.app',
     storageBucket: 'capstone-343210.appspot.com',
     locationId: 'asia-southeast1',
     apiKey: 'AIzaSyB2dfy6teqwHrQqX7Gso3kUPHaZ_dEe8-c',
@@ -14,8 +15,8 @@ export const environment = {
     messagingSenderId: '1022240410262',
     measurementId: 'G-PN4KKHYTQJ',
   },
-  // API_URL: 'https://capstone-hbts.herokuapp.com/api/v1',
-  API_URL: 'http://localhost:8080/api/v1',
+  API_URL: 'https://capstone-hbts.herokuapp.com/api/v1',
+  // API_URL: 'http://localhost:8080/api/v1',
   production: true,
 };
 
