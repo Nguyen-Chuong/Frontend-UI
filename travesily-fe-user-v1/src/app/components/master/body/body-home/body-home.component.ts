a5758ac7282e2b2ef7002aba9f883d886de438dd
hoanbui29 <81216107+hoanbui29@users.noreply.github.com>
2022-04-14 20:57:02 +0700

    Update fix coupon
    Update fix coupon

--- travesily-fe-user-v1/src/app/components/master/body/body-home/body-home.component.ts
+++ travesily-fe-user-v1/src/app/components/master/body/body-home/body-home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { CouponDialogComponent } from 'src/app/shared/components/coupon-dialog/coupon-dialog.component';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {CouponDialogComponent} from 'src/app/shared/components/coupon-dialog/coupon-dialog.component';
+import {StorageService} from "../../../../_services/storage.service";
+import {AuthService} from "../../../../_services/auth.service";
 
 @Component({
   selector: 'app-body-home',
@@ -8,10 +10,18 @@ import { CouponDialogComponent } from 'src/app/shared/components/coupon-dialog/c
   styleUrls: ['./body-home.component.scss']
 })
 export class BodyHomeComponent implements OnInit {
-  constructor(public dialog: MatDialog) {
+  constructor(private dialog: MatDialog,
+              private storage: StorageService,
+              private authService: AuthService) {
+    if(this.storage.authToken)
+    this.authService.getProfile().subscribe({
+      next: value => {
+        this.dialog.open(CouponDialogComponent);
+      }
+    })
   }
 
   ngOnInit(): void {
-    this.dialog.open(CouponDialogComponent);
+
   }
 }
