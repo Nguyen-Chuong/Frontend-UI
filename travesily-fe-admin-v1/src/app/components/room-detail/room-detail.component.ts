b5f74099f7f6953d7bededbfb3860762855443e4
Viet Hoang <Dragonnb001@gmail.com>
2022-04-14 13:06:16 +0700

    add carousel image for room type

--- travesily-fe-admin-v1/src/app/components/room-detail/room-detail.component.ts
+++ travesily-fe-admin-v1/src/app/components/room-detail/room-detail.component.ts
@@ -1,7 +1,7 @@
 import { Image } from './../../_models/image';
 import { Component, Input, OnInit } from '@angular/core';
 import { Room } from 'src/app/_models/room';
-
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-room-detail',
   templateUrl: './room-detail.component.html',
@@ -12,13 +12,20 @@ export class RoomDetailComponent implements OnInit {
 
   images: Image[]
   imageSrc: string
-  constructor() { }
+  modal = ''
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.keyboard = false;
+  }
   ngOnInit(): void {
+
     this.images = this.room.listImage
     if (this.images.length === 0) {
       this.imageSrc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt1R-elWRjhm7X8oDey-GQJeZa9fiPCT_QkA&usqp=CAU'
     } else {
       this.imageSrc = this.images[0].src
     }
+  }
+  open(content) {
+    this.modalService.open(content);
   }
 }
